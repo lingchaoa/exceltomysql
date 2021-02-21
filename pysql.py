91f0fd136c51aaa164dd80e7793294190a0a416a
konglingchao <13165317531@163.com>
2021-02-21 21:02:17 +0800

    222

--- pysql.py
+++ pysql.py
@@ -106,8 +106,8 @@ if __name__ == "__main__":
 
     ## 2.新增
     # 返回受影响行数
-    print(data_base.execute_modify("insert into users(name,age,description) values (%s,%s,%s)",
-                                   ['gpw', '18', 'This is author']))
+    print(data_base.execute_modify("insert into city_nation values (%s,%s)",
+                                   ['gpw','author']))
 
 
 
